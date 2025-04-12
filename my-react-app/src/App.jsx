f2db14485937862246868a9b20c8a9ade47fb7e7
Simon <simonbakkenjantasuk@pm.me>
2025-04-13 01:14:16 +0200

    Legg til layout

--- my-react-app/src/App.jsx
+++ my-react-app/src/App.jsx
@@ -1,7 +1,16 @@
+import RegisterBook from "./components/RegisterBook";
+import Books from "./components/Books";
 import { useState } from "react";
 
 function App() {
-  return <div></div>;
+  const [books, setBooks] = useState([]);
+
+  return (
+    <div>
+      <RegisterBook books={books} setBooks={setBooks} />
+      <Books books={books} setBooks={setBooks} />
+    </div>
+  );
 }
 
 export default App;
